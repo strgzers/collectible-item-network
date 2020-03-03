2406d623434b8cc61bdeb1f04b36cb9892f8a3de
jdshepler <jshepler@umich.edu>
2020-03-03 11:47:03 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Collectible Item Network
         </p>
         <a
           className="App-link"

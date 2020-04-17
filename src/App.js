f178159ca9aeebac5ca395840a87418d72cd7f72
jdshepler <jshepler@umich.edu>
2020-04-17 10:02:51 -0400

    Implementing Material-UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Counter } from './components/Counter';
-import { NavMenu } from './components/NavMenu';
+import Nav from './components/Nav';
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 import './containers/App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="container">
-        <h1 id="brand">Collectible Item Network</h1>
-        <h3 id="navbar"><NavMenu /></h3>
-      </div>
-      <div>
-        <Switch>
-          <Route path='/' component={Home} exact />
-          <Route path='/counter' component={Counter} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/sign-in' component={SignIn} />
+                    <Route path='/sign-up' component={SignUp} />
+                    <Route path='/counter' component={Counter} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
